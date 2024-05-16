b60ef00fc0c36ad4502fe68c533a4443bb1942c8
rohith887 <rohithreddy1325@gmail.com>
2024-05-16 20:20:00 +0530

    first commit on web dev

---
+++ zerodha-app/main.js
@@ -0,0 +1,29 @@
+/*console.log("hello universe");
+console.log(a);
+var a=1;
+console.log(a);*/
+
+/*let firstname= "harikrat";
+let age1="28";
+let isMarried1= true;
+let lastname = "koms";
+let age2 = "38";
+let isMarried2 = false;
+console.log("this person's name is " +firstname +"and their age is"+age1);  
+console.log("that person's name is ");
+console.log(firstname);
+console.log("this person's name is " + lastname + "and their age is" + age2);
+console.log("that person's name is ");
+console.log(lastname);
+*/
+/*
+let firstname = "harikrat";
+let age1 = "28";
+let isMarried = true;
+if (isMarried==true)
+    console.log(firstname + "is not married");
+else
+    console.log(firstname + "is married");
+   */ 
+
+let ans
